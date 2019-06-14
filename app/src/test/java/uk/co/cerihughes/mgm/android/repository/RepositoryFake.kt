df1cec1bf6459ee38e1ed26231b1c02950577c61
Ceri Hughes <github@cerihughes.co.uk>
2019-06-14 19:38:31 +0100

    Don't test with Koin

---
+++ app/src/test/java/uk/co/cerihughes/mgm/android/repository/RepositoryFake.kt
@@ -0,0 +1,11 @@
+package uk.co.cerihughes.mgm.android.repository
+
+import uk.co.cerihughes.mgm.android.model.Event
+
+class RepositoryFake : Repository {
+    val getEventsData: List<Event> = emptyList()
+
+    override fun getEvents(callback: Repository.GetEventsCallback) {
+        callback.onEventsLoaded(getEventsData)
+    }
+}
