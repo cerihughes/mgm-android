df6e9beebefc0f1a119be2e0fdea2465fd7d40f9
Ceri Hughes <github@cerihughes.co.uk>
2019-06-15 16:46:01 +0100

    Fix tests

--- app/src/androidTest/java/uk/co/cerihughes/mgm/android/repository/local/LocalDataSourceImplTest.kt
+++ app/src/androidTest/java/uk/co/cerihughes/mgm/android/repository/local/LocalDataSourceImplTest.kt
@@ -1,8 +1,11 @@
 package uk.co.cerihughes.mgm.android.repository.local
 
+import androidx.test.platform.app.InstrumentationRegistry
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 
 class LocalDataSourceImplTest {
 
@@ -10,6 +13,8 @@ class LocalDataSourceImplTest {
 
     @Before
     fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        Realm.init(context)
         val config = RealmConfiguration.Builder()
             .name("LocalDataSourceImplTest.realm")
             .inMemory()
@@ -17,5 +22,10 @@ class LocalDataSourceImplTest {
         val realm = Realm.getInstance(config)
 
         localDataSource = LocalDataSourceImpl(realm)
+    }
+
+    @Test
+    fun placeholder() {
+        Assert.assertTrue(true)
     }
 }
