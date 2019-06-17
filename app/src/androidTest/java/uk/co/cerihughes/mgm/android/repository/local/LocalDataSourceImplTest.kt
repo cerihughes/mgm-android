890e849db8eb4c5e54fe8f7b63cb5d92f6e9eeff
Ceri Hughes <github@cerihughes.co.uk>
2019-06-17 08:15:11 +0100

    Try commenting out annotations

--- app/src/androidTest/java/uk/co/cerihughes/mgm/android/repository/local/LocalDataSourceImplTest.kt
+++ app/src/androidTest/java/uk/co/cerihughes/mgm/android/repository/local/LocalDataSourceImplTest.kt
@@ -11,7 +11,7 @@ class LocalDataSourceImplTest {
 
     private lateinit var localDataSource: LocalDataSource
 
-    @Before
+//    @Before
     fun setUp() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         Realm.init(context)
@@ -24,7 +24,7 @@ class LocalDataSourceImplTest {
         localDataSource = LocalDataSourceImpl(realm)
     }
 
-    @Test
+//    @Test
     fun placeholder() {
         Assert.assertTrue(true)
     }
